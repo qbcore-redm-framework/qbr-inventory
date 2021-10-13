0ad49e94273869434d8a112ec8ab43e1d8170266
GhzGarage <gigahertzgarage@mail.com>
2021-10-12 21:31:20 -0500

    Initial Commit

---
+++ config.lua
@@ -0,0 +1,250 @@
+Config = {}
+
+local StringCharset = {}
+local NumberCharset = {}
+
+for i = 48,  57 do table.insert(NumberCharset, string.char(i)) end
+for i = 65,  90 do table.insert(StringCharset, string.char(i)) end
+for i = 97, 122 do table.insert(StringCharset, string.char(i)) end
+
+Config.RandomStr = function(length)
+	if length > 0 then
+		return Config.RandomStr(length-1) .. StringCharset[math.random(1, #StringCharset)]
+	else
+		return ''
+	end
+end
+
+Config.RandomInt = function(length)
+	if length > 0 then
+		return Config.RandomInt(length-1) .. NumberCharset[math.random(1, #NumberCharset)]
+	else
+		return ''
+	end
+end
+
+Config.VendingObjects = {
+    "prop_vend_soda_01",
+    "prop_vend_soda_02",
+    "prop_vend_water_01"
+}
+
+Config.BinObjects = {
+    "prop_bin_05a",
+}
+
+Config.VendingItem = {
+    [1] = {
+        name = "kurkakola",
+        price = 4,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+    [2] = {
+        name = "water_bottle",
+        price = 4,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 2,
+    },
+}
+
+Config.CraftingItems = {
+    [1] = {
+        name = "lockpick",
+        amount = 50,
+        info = {},
+        costs = {
+            ["metalscrap"] = 22,
+            ["plastic"] = 32,
+        },
+        type = "item",
+        slot = 1,
+        threshold = 0,
+        points = 1,
+    },
+    [2] = {
+        name = "screwdriverset",
+        amount = 50,
+        info = {},
+        costs = {
+            ["metalscrap"] = 30,
+            ["plastic"] = 42,
+        },
+        type = "item",
+        slot = 2,
+        threshold = 0,
+        points = 2,
+    },
+}
+
+Config.AttachmentCrafting = {
+    ["location"] = {x = 88.91, y = 3743.88, z = 40.77, h = 66.5, r = 1.0}, 
+    ["items"] = {
+        [1] = {
+            name = "pistol_extendedclip",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 140,
+                ["steel"] = 250,
+                ["rubber"] = 60,
+            },
+            type = "item",
+            slot = 1,
+            threshold = 0,
+            points = 1,
+        },
+        [2] = {
+            name = "pistol_suppressor",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 165,
+                ["steel"] = 285,
+                ["rubber"] = 75,
+            },
+            type = "item",
+            slot = 2,
+            threshold = 10,
+            points = 2,
+        },
+        [3] = {
+            name = "rifle_extendedclip",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 190,
+                ["steel"] = 305,
+                ["rubber"] = 85,
+                ["smg_extendedclip"] = 1,
+            },
+            type = "item",
+            slot = 7,
+            threshold = 25,
+            points = 8,
+        },
+        [4] = {
+            name = "rifle_drummag",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 205,
+                ["steel"] = 340,
+                ["rubber"] = 110,
+                ["smg_extendedclip"] = 2,
+            },
+            type = "item",
+            slot = 8,
+            threshold = 50,
+            points = 8,
+        },
+        [5] = {
+            name = "smg_flashlight",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 230,
+                ["steel"] = 365,
+                ["rubber"] = 130,
+            },
+            type = "item",
+            slot = 3,
+            threshold = 75,
+            points = 3,
+        },
+        [6] = {
+            name = "smg_extendedclip",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 255,
+                ["steel"] = 390,
+                ["rubber"] = 145,
+            },
+            type = "item",
+            slot = 4,
+            threshold = 100,
+            points = 4,
+        },
+        [7] = {
+            name = "smg_suppressor",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 270,
+                ["steel"] = 435,
+                ["rubber"] = 155,
+            },
+            type = "item",
+            slot = 5,
+            threshold = 150,
+            points = 5,
+        },
+        [8] = {
+            name = "smg_scope",
+            amount = 50,
+            info = {},
+            costs = {
+                ["metalscrap"] = 300,
+                ["steel"] = 469,
+                ["rubber"] = 170,
+            },
+            type = "item",
+            slot = 6,
+            threshold = 200,
+            points = 6,
+        },
+    }
+}
+
+MaxInventorySlots = 41
+
+BackEngineVehicles = {
+    'ninef',
+    'adder',
+    'vagner',
+    't20',
+    'infernus',
+    'zentorno',
+    'reaper',
+    'comet2',
+    'comet3',
+    'jester',
+    'jester2',
+    'cheetah',
+    'cheetah2',
+    'prototipo',
+    'turismor',
+    'pfister811',
+    'ardent',
+    'nero',
+    'nero2',
+    'tempesta',
+    'vacca',
+    'bullet',
+    'osiris',
+    'entityxf',
+    'turismo2',
+    'fmj',
+    're7b',
+    'tyrus',
+    'italigtb',
+    'penetrator',
+    'monroe',
+    'ninef2',
+    'stingergt',
+    'surfer',
+    'surfer2',
+    'comet3',
+}
+
+Config.MaximumAmmoValues = {
+    ["pistol"] = 250,
+    ["smg"] = 250,
+    ["shotgun"] = 200,
+    ["rifle"] = 250,
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'QB-Inventory'
+version '1.0.0'
+
+shared_scripts {
+	'config.lua'
+}
+
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/main.css',
+	'html/js/app.js',
+	'html/images/*.png',
+	'html/images/*.jpg',
+	'html/ammo_images/*.png',
+	'html/attachment_images/*.png',
+	'html/*.ttf'
+}
